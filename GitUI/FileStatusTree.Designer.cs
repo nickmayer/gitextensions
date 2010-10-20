bc49d67544fac2edae45f413e59b016410d4e6aa
Nick Mayer <nicholas@the-mayers.net>
2010-10-20 11:59:10 -0500

    Tree shows all files

--- GitUI/FileStatusTree.Designer.cs
+++ GitUI/FileStatusTree.Designer.cs
@@ -28,22 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.treeView = new System.Windows.Forms.TreeView();
             this.SuspendLayout();
             // 
-            // treeView1
+            // treeView
             // 
-            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.treeView1.Location = new System.Drawing.Point(0, 0);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(150, 150);
-            this.treeView1.TabIndex = 0;
+            this.treeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView.FullRowSelect = true;
+            this.treeView.HideSelection = false;
+            this.treeView.Location = new System.Drawing.Point(0, 0);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(150, 150);
+            this.treeView.TabIndex = 0;
+            this.treeView.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView_BeforeExpand);
             // 
             // FileStatusTree
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.treeView);
             this.Name = "FileStatusTree";
             this.ResumeLayout(false);
 
@@ -51,6 +54,6 @@
 
         #endregion
 
-        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TreeView treeView;
     }
 }
--- GitUI/FileStatusTree.cs
+++ GitUI/FileStatusTree.cs
@@ -5,22 +5,107 @@ using System.Drawing;
 using System.Data;
 using System.Text;
 using System.Windows.Forms;
+using GitCommands;
 
 namespace GitUI
 {
     public partial class FileStatusTree : GitExtensionsControl
     {
         private readonly IndexWatcher _indexWatcher = new IndexWatcher();
+        
+        public bool IsFolderFirst = true;
+
+        private class NodeTag
+        {
+            public NodeTag()
+            {
+                GitItem = null;
+                IsPopulated = false;
+            }
+            public NodeTag(IGitItem aItem)
+            {
+                GitItem = aItem;
+                IsPopulated = false;
+            }
+
+            public IGitItem GitItem;
+            public bool IsPopulated;
+        }
 
         public FileStatusTree()
         {
             InitializeComponent();
             Translate();
             _indexWatcher.Changed += new IndexWatcher.ChangeEventHandler(IndexWatcher_onIndexChanged);
+            BuildTree();
         }
 
         private void IndexWatcher_onIndexChanged()
         {
+            // TOOD: If our tree SHA changes, we'll need to rebuild the entire tree.
+        }
+
+        private void UpdateStatus()
+        {
+
+        }
+
+        private void BuildTree()
+        {
+            TreeNode root = treeView.Nodes.Add("HEAD");
+            root.Tag = new NodeTag();
+            root.Nodes.Add(new TreeNode());
+            root.Expand();
+        }
+
+        private void treeView_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            NodeTag tag = e.Node.Tag as NodeTag;
+            if (tag.IsPopulated)
+            {
+                return;
+            }
+            treeView.SuspendLayout();
+
+            tag.IsPopulated = true;
+            e.Node.Nodes.Clear();
+
+            List<IGitItem> items;
+            if (tag.GitItem == null)
+            {
+                items = GitCommands.GitCommands.GetTree("HEAD");
+            }
+            else
+            {
+                items = tag.GitItem.SubItems;
+            }
+
+            int folderIndex = 0;
+            foreach (IGitItem gitNode in items)
+            {
+                TreeNode treeNode = new TreeNode(gitNode.Name);
+                treeNode.Tag = new NodeTag(gitNode);
+
+                GitItem gitItem = gitNode as GitItem;
+                bool isFolder = gitItem == null || gitItem.ItemType == "tree" || gitItem.ItemType == "commit";
+                if (isFolder)
+                {   
+                    treeNode.Nodes.Add(new TreeNode());
+                    isFolder = true;
+                }
+
+                if (IsFolderFirst && isFolder)
+                {
+                    e.Node.Nodes.Insert(folderIndex, treeNode);
+                    folderIndex++;
+                }
+                else
+                {
+                    e.Node.Nodes.Add(treeNode);
+                }
+            }            
+
+            treeView.ResumeLayout();
         }
     }
 }
